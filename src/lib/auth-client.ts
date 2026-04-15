900bd141e070f5e90c55566b62d670463c2b413c
Cho Yin Yong <choyiny@users.noreply.github.com>
2026-04-15 02:28:49 -0400

    feat: add passkeyClient plugin to frontend auth client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,10 @@
 import { createAuthClient } from "better-auth/react";
 import { adminClient } from "better-auth/client/plugins";
+import { passkeyClient } from "@better-auth/passkey/client";
 
 export const authClient = createAuthClient({
   baseURL: window.location.origin,
-  plugins: [adminClient()],
+  plugins: [adminClient(), passkeyClient()],
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
